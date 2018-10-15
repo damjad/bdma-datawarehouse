0ec52665fd917924a5e8eb9ef514bcee44727fe1
danish.amjad <danish.amjad@seamless.se>
2018-10-15 20:38:44 +0200

    updated scripts, removed extra error file

--- tpc-ds-postgres/db-mysql.sh
+++ tpc-ds-postgres/db-mysql.sh
@@ -1,3 +1,13 @@
 mysql -urefill -prefill -Dtpcds -e "drop database tpcds; create database tpcds;"mysql -urefill -prefill -Dtpcds < tpcds.sql
-bash /tmp/load-tcp-data.sh
+DIR=/tmp/dsd
+ls $DIR/*.dat | while read file; do
+    pipe=$file.pipe
+    mkfifo $pipe
+    table=`basename $file .dat | sed -e 's/_[0-9]_[0-9]//'`
+    echo $file $table
+    LANG=C && sed -e ':loop' -e 's_^|_\\N|_g' -e 's_||_|\\N|_g' -e 's_||$_|\\N|_g' -e 't loop' $file > $pipe & \
+    mysql --local-infile -Dtpcds -e \
+          "load data local infile '$pipe' replace into table $table character set latin1 fields terminated by '|'"
+    rm -f $pipe
+done
 
